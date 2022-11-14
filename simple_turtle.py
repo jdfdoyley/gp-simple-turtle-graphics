9b8358b4a5066edf9c17e2f9b2465fe155c57771
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-13 20:18:22 -0500

    Demonstrate the turtle module

---
+++ simple_turtle.py
@@ -0,0 +1,35 @@
+###############################################################################
+# Name: Jason D. F. D'Oyley
+# Date: November 13, 2022
+# Assignement: Guide Practice 1 - Design, Write, Test, and Correct a Program
+# Task 3 - Simple Turtle Graphics
+#
+# Description: Write a program to only print out even numbers.
+###############################################################################
+import turtle
+
+turtle.write("JASDOY5736")
+
+# Octagon Shape
+# turtle.forward(50)
+# turtle.right(45)
+# turtle.forward(50)
+# turtle.right(45)
+# turtle.forward(50)
+# turtle.right(45)
+# turtle.forward(50)
+# turtle.right(45)
+# turtle.forward(50)
+# turtle.right(45)
+# turtle.forward(50)
+# turtle.right(45)
+# turtle.forward(50)
+# turtle.right(45)
+# turtle.forward(50)
+
+# Circle Shape
+turtle.pencolor("tomato")
+turtle.circle(50)
+
+# turtle.done # Does not keep the window open
+turtle.exitonclick()
